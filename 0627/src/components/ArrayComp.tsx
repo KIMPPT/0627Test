bddb56813bad582b928becf346e6314337571ed2
KIMPPT <dudghks9303@gmail.com>
2023-06-27 10:48:10 +0900

    06271048

--- 0627/src/components/ArrayComp.tsx
+++ 0627/src/components/ArrayComp.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React, { useState } from "react";
+interface list {
+  id: string;
+  input: string;
+}
 export default function ArrayComp() {
+  const [listarray, setListarray] = useState<[list, list, list]>([
+    { id: "id", input: "text" },
+    { id: "1", input: "one" },
+    { id: "2", input: "two" },
+  ]);
   return (
     <div>
-        <hr />
+      <hr />
+      {listarray.map((data)=><li>
+        {data.id}:{data.input}
+      </li>)}
     </div>
-  )
+  );
 }
